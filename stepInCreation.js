f720e437266f0738e3709fea19303c5f2f50bbb5
MOSES <mosecarboncode@gmail.com>
2025-01-13 14:46:06 +0100

    added this step in creation file to the rest of the files

---
+++ stepInCreation.js
@@ -0,0 +1,4 @@
+    //1. Creating nad styling buttons
+    //2. declaring functions in each button
+    //3.creating and initializing the functions in js
+        //. This was actually fairly simple
